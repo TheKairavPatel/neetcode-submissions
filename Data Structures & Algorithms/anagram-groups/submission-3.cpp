f942e52f23402c87a63b70f31c56090e8e10f88d
Kairav Patel <kairavpatel13@gmail.com>
2026-09-21 13:43:09 -0500

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mapper;
+        for (auto it = strs.begin(); it != strs.end(); ++it)
+        {
+            int king[26] = {0};
+            for (auto it2 = (*it).begin(); it2 != (*it).end(); ++it2)
+            {
+                king[*it2 - 97]++;
+            }
+            string key{};
+            for (int i = 0; i < 26; i++)
+            {
+                key += to_string(king[i]);
+                key += ",";
+            }
+            auto it3 = mapper.find(key);
+            mapper[key].push_back(*it);
+
+        }
+        vector<vector<string>> output{};
+        int x = 0;
+        for (auto &[key, strings] : mapper)
+        {
+            output.insert(output.begin() + x, strings);
+            x++;
+        }
+        return output;
+    }
+};
